ce0b863cf0666b70374469c8b8ef48309533efe9
Joseph Martinez <joseph.r.martinez@gmail.com>
2022-11-07 19:51:24 -0700

    set up webpack and initial file structure

---
+++ src/index.js
@@ -0,0 +1,6 @@
+// call the initial page load function
+
+// write tab switching logic. there should be
+// event listeners on each tab that wipe out current
+// content in <div id="content"> and then runs the correct
+// tab module to populate the <div id="content">
